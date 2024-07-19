37f0e3df3d9990247d76d504929be86f3c7b1f99
SJU <soulsdhed@gmail.com>
2024-07-19 17:07:32 +0900

    init2

---
+++ routes/api/bloodlog.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    // get
+});
+
+router.post("/", (req, res) => {
+    // post
+});
+
+module.exports = router;

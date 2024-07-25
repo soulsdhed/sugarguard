1f62c82058c57ed8407d9dc28b03ef4dd2485212
JJS <jjs0601015@yahoo.com>
2024-07-25 14:01:52 +0900

    z

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -24,8 +24,11 @@ router.get("/recipe", (req, res) => {
 router.get("/sugardiary", (req, res) => {
     res.render("sugardiary");
 });
-router.get("/recipe_details", (req,res)=> {
-    res.render("recipe_details")
-})
+router.get("/recipe_details", (req, res) => {
+    res.render("recipe_details");
+});
+router.get("/report/:type", (req, res) => {
+    res.render("report");
+});
 
 module.exports = router;

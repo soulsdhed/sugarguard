bc0c0feec643dd156c23763269559d744fb8cc2e
SJU <soulsdhed@gmail.com>
2024-07-25 12:02:03 +0900

    api complete

--- routes/api/chart.js
+++ routes/api/chart.js
@@ -83,8 +83,7 @@ router.get("/exercise", authenticateToken, (req, res, next) => {
             SELECT 
                 SUM(calories_burned) AS total_calories,
                 AVG(calories_burned) AS average_calories_per_entry,
-                DATE_FORMAT(record_time, ?) AS period,
-                COUNT(DISTINCT DATE(record_time)) AS entry_count
+                DATE_FORMAT(record_time, ?) AS period
             FROM EXERCISE_LOG_TB
             WHERE record_time BETWEEN ? AND ?
             GROUP BY period
@@ -107,10 +106,9 @@ router.get("/exercise", authenticateToken, (req, res, next) => {
                         period: row.period,
                         total_calories: row.total_calories,
                         average_calories_per_day: row.total_calories / days,
-                        days: days,
                         average_calories_per_entry:
                             row.average_calories_per_entry,
-                        entry_count: row.entry_count,
+                        days: days,
                     };
                 });
 
@@ -128,6 +126,464 @@ router.get("/exercise", authenticateToken, (req, res, next) => {
     }
 });
 
-// TODO :
+// 혈당 차트 (일/주/월)
+router.get("/blood-sugar", authenticateToken, (req, res, next) => {
+    const { userId } = req.user;
+    let {
+        period,
+        startDate,
+        endDate,
+        startWeek,
+        endWeek,
+        startMonth,
+        endMonth,
+    } = req.query;
+
+    // period 유효성 검사
+    if (!["DAY", "WEEK", "MONTH"].includes(period.toUpperCase())) {
+        return next({
+            code: "VALIDATION_ERROR",
+        });
+    }
+
+    // 각 period별로 query 유효성 검사
+    if (period.toUpperCase() == "DAY") {
+        if (!isValidDate(startDate) || !isValidDate(endDate)) {
+            return next({
+                code: "VALIDATION_ERROR",
+            });
+        }
+    } else if (period.toUpperCase() == "WEEK") {
+        if (!isValidWeek(startWeek) || !isValidWeek(endWeek)) {
+            return next({
+                code: "VALIDATION_MISSING_FIELD",
+            });
+        }
+        const date = getWeekDateRange(startWeek, endWeek);
+        startDate = date.startDate;
+        endDate = date.endDate;
+    } else if (period.toUpperCase() == "MONTH") {
+        if (!isValidMonth(startMonth) || !isValidMonth(endMonth)) {
+            return next({
+                code: "VALIDATION_MISSING_FIELD",
+            });
+        }
+        startDate = startMonth + "-01";
+        endDate = getLastDateOfMonth(endMonth);
+    }
+
+    // startDate가 endDate보다 더 뒤인 경우
+    if (new Date(startDate) > new Date(endDate)) {
+        return next({
+            code: "VALIDATION_ERROR",
+        });
+    }
+
+    const startDateTime = `${startDate} 00:00:00`;
+    const endDateTime = `${endDate} 23:59:59`;
+
+    let dateFormat = "";
+    if (period.toUpperCase() === "DAY") {
+        dateFormat = "%Y-%m-%d";
+    } else if (period.toUpperCase() === "WEEK") {
+        dateFormat = "%Y-%u";
+    } else if (period.toUpperCase() === "MONTH") {
+        dateFormat = "%Y-%m";
+    }
+
+    try {
+        const query = `
+            SELECT 
+                SUM(blood_sugar) AS total_blood_sugar,
+                AVG(blood_sugar) AS average_blood_sugar_per_entry,
+                DATE_FORMAT(record_time, ?) AS period
+            FROM BLOOD_SUGAR_LOG_TB 
+            WHERE record_time BETWEEN ? AND ?
+            GROUP BY period
+            ORDER BY period
+        `;
+        db.execute(
+            query,
+            [dateFormat, startDateTime, endDateTime],
+            (err, rows) => {
+                if (err) {
+                    return next({
+                        code: "SERVER_INTERNAL_ERROR",
+                    });
+                }
+
+                // 결과값 생성
+                const results = rows.map((row) => {
+                    const days = calculateDays(period, row.period);
+                    return {
+                        period: row.period,
+                        total_blood_sugar: row.total_blood_sugar,
+                        average_blood_sugar_per_day:
+                            row.total_blood_sugar / days,
+                        average_blood_sugar_per_entry:
+                            row.average_blood_sugar_per_entry,
+                        days: days,
+                    };
+                });
+
+                return res.success({
+                    userId: userId,
+                    count: rows.length,
+                    chartData: results,
+                });
+            }
+        );
+    } catch (e) {
+        return next({
+            code: "SERVER_INTERNAL_ERROR",
+        });
+    }
+});
+
+// 체중 일/주/월 차트
+router.get("/weight", authenticateToken, (req, res, next) => {
+    const { userId } = req.user;
+    let {
+        period,
+        startDate,
+        endDate,
+        startWeek,
+        endWeek,
+        startMonth,
+        endMonth,
+    } = req.query;
+
+    // period 유효성 검사
+    if (!["DAY", "WEEK", "MONTH"].includes(period.toUpperCase())) {
+        return next({
+            code: "VALIDATION_ERROR",
+        });
+    }
+
+    // 각 period별로 query 유효성 검사
+    if (period.toUpperCase() == "DAY") {
+        if (!isValidDate(startDate) || !isValidDate(endDate)) {
+            return next({
+                code: "VALIDATION_ERROR",
+            });
+        }
+    } else if (period.toUpperCase() == "WEEK") {
+        if (!isValidWeek(startWeek) || !isValidWeek(endWeek)) {
+            return next({
+                code: "VALIDATION_MISSING_FIELD",
+            });
+        }
+        const date = getWeekDateRange(startWeek, endWeek);
+        startDate = date.startDate;
+        endDate = date.endDate;
+    } else if (period.toUpperCase() == "MONTH") {
+        if (!isValidMonth(startMonth) || !isValidMonth(endMonth)) {
+            return next({
+                code: "VALIDATION_MISSING_FIELD",
+            });
+        }
+        startDate = startMonth + "-01";
+        endDate = getLastDateOfMonth(endMonth);
+    }
+
+    // startDate가 endDate보다 더 뒤인 경우
+    if (new Date(startDate) > new Date(endDate)) {
+        return next({
+            code: "VALIDATION_ERROR",
+        });
+    }
+
+    const startDateTime = `${startDate} 00:00:00`;
+    const endDateTime = `${endDate} 23:59:59`;
+
+    let dateFormat = "";
+    if (period.toUpperCase() === "DAY") {
+        dateFormat = "%Y-%m-%d";
+    } else if (period.toUpperCase() === "WEEK") {
+        dateFormat = "%Y-%u";
+    } else if (period.toUpperCase() === "MONTH") {
+        dateFormat = "%Y-%m";
+    }
+
+    try {
+        const query = `
+            SELECT 
+                SUM(weight) AS total_weight,
+                AVG(weight) AS average_weight_per_entry,
+                DATE_FORMAT(record_time, ?) AS period
+            FROM WEIGHT_LOG_TB
+            WHERE record_time BETWEEN ? AND ?
+            GROUP BY period
+            ORDER BY period
+        `;
+        db.execute(
+            query,
+            [dateFormat, startDateTime, endDateTime],
+            (err, rows) => {
+                if (err) {
+                    return next({
+                        code: "SERVER_INTERNAL_ERROR",
+                    });
+                }
+
+                // 결과값 생성
+                const results = rows.map((row) => {
+                    const days = calculateDays(period, row.period);
+                    return {
+                        period: row.period,
+                        total_weight: row.total_weight,
+                        average_weight_per_day: row.total_weight / days,
+                        average_weight_per_entry: row.average_weight_per_entry,
+                        days: days,
+                    };
+                });
+
+                return res.success({
+                    userId: userId,
+                    count: rows.length,
+                    chartData: results,
+                });
+            }
+        );
+    } catch (e) {
+        return next({
+            code: "SERVER_INTERNAL_ERROR",
+        });
+    }
+});
+
+// 혈압 일/주/월 차트
+router.get("/blood-pressure", authenticateToken, (req, res, next) => {
+    const { userId } = req.user;
+    let {
+        period,
+        startDate,
+        endDate,
+        startWeek,
+        endWeek,
+        startMonth,
+        endMonth,
+    } = req.query;
+
+    // period 유효성 검사
+    if (!["DAY", "WEEK", "MONTH"].includes(period.toUpperCase())) {
+        return next({
+            code: "VALIDATION_ERROR",
+        });
+    }
+
+    // 각 period별로 query 유효성 검사
+    if (period.toUpperCase() == "DAY") {
+        if (!isValidDate(startDate) || !isValidDate(endDate)) {
+            return next({
+                code: "VALIDATION_ERROR",
+            });
+        }
+    } else if (period.toUpperCase() == "WEEK") {
+        if (!isValidWeek(startWeek) || !isValidWeek(endWeek)) {
+            return next({
+                code: "VALIDATION_MISSING_FIELD",
+            });
+        }
+        const date = getWeekDateRange(startWeek, endWeek);
+        startDate = date.startDate;
+        endDate = date.endDate;
+    } else if (period.toUpperCase() == "MONTH") {
+        if (!isValidMonth(startMonth) || !isValidMonth(endMonth)) {
+            return next({
+                code: "VALIDATION_MISSING_FIELD",
+            });
+        }
+        startDate = startMonth + "-01";
+        endDate = getLastDateOfMonth(endMonth);
+    }
+
+    // startDate가 endDate보다 더 뒤인 경우
+    if (new Date(startDate) > new Date(endDate)) {
+        return next({
+            code: "VALIDATION_ERROR",
+        });
+    }
+
+    const startDateTime = `${startDate} 00:00:00`;
+    const endDateTime = `${endDate} 23:59:59`;
+
+    let dateFormat = "";
+    if (period.toUpperCase() === "DAY") {
+        dateFormat = "%Y-%m-%d";
+    } else if (period.toUpperCase() === "WEEK") {
+        dateFormat = "%Y-%u";
+    } else if (period.toUpperCase() === "MONTH") {
+        dateFormat = "%Y-%m";
+    }
+
+    try {
+        const query = `
+            SELECT 
+                SUM(blood_pressure_min) AS total_blood_pressure_min,
+                AVG(blood_pressure_min) AS average_blood_pressure_min_per_entry,
+                SUM(blood_pressure_max) AS total_blood_pressure_max,
+                AVG(blood_pressure_max) AS average_blood_pressure_max_per_entry,
+                DATE_FORMAT(record_time, ?) AS period
+            FROM BLOOD_PRESSURE_LOG_TB
+            WHERE record_time BETWEEN ? AND ?
+            GROUP BY period
+            ORDER BY period
+        `;
+        db.execute(
+            query,
+            [dateFormat, startDateTime, endDateTime],
+            (err, rows) => {
+                if (err) {
+                    return next({
+                        code: "SERVER_INTERNAL_ERROR",
+                    });
+                }
+
+                // 결과값 생성
+                const results = rows.map((row) => {
+                    const days = calculateDays(period, row.period);
+                    return {
+                        period: row.period,
+                        total_blood_pressure_min: row.total_blood_pressure_min,
+                        average_blood_pressure_min_per_day:
+                            row.total_blood_pressure_min / days,
+                        average_blood_pressure_min_per_entry:
+                            row.average_blood_pressure_min_per_entry,
+                        total_blood_pressure_max: row.total_blood_pressure_max,
+                        average_blood_pressure_max_per_day:
+                            row.total_blood_pressure_max / days,
+                        average_blood_pressure_max_per_entry:
+                            row.average_blood_pressure_max_per_entry,
+                        days: days,
+                    };
+                });
+
+                return res.success({
+                    userId: userId,
+                    count: rows.length,
+                    chartData: results,
+                });
+            }
+        );
+    } catch (e) {
+        return next({
+            code: "SERVER_INTERNAL_ERROR",
+        });
+    }
+});
+
+// 식사 섭취 칼로리 일/주/월 차트
+router.get("/meal", authenticateToken, (req, res, next) => {
+    const { userId } = req.user;
+    let {
+        period,
+        startDate,
+        endDate,
+        startWeek,
+        endWeek,
+        startMonth,
+        endMonth,
+    } = req.query;
+
+    // period 유효성 검사
+    if (!["DAY", "WEEK", "MONTH"].includes(period.toUpperCase())) {
+        return next({
+            code: "VALIDATION_ERROR",
+        });
+    }
+
+    // 각 period별로 query 유효성 검사
+    if (period.toUpperCase() == "DAY") {
+        if (!isValidDate(startDate) || !isValidDate(endDate)) {
+            return next({
+                code: "VALIDATION_ERROR",
+            });
+        }
+    } else if (period.toUpperCase() == "WEEK") {
+        if (!isValidWeek(startWeek) || !isValidWeek(endWeek)) {
+            return next({
+                code: "VALIDATION_MISSING_FIELD",
+            });
+        }
+        const date = getWeekDateRange(startWeek, endWeek);
+        startDate = date.startDate;
+        endDate = date.endDate;
+    } else if (period.toUpperCase() == "MONTH") {
+        if (!isValidMonth(startMonth) || !isValidMonth(endMonth)) {
+            return next({
+                code: "VALIDATION_MISSING_FIELD",
+            });
+        }
+        startDate = startMonth + "-01";
+        endDate = getLastDateOfMonth(endMonth);
+    }
+
+    // startDate가 endDate보다 더 뒤인 경우
+    if (new Date(startDate) > new Date(endDate)) {
+        return next({
+            code: "VALIDATION_ERROR",
+        });
+    }
+
+    const startDateTime = `${startDate} 00:00:00`;
+    const endDateTime = `${endDate} 23:59:59`;
+
+    let dateFormat = "";
+    if (period.toUpperCase() === "DAY") {
+        dateFormat = "%Y-%m-%d";
+    } else if (period.toUpperCase() === "WEEK") {
+        dateFormat = "%Y-%u";
+    } else if (period.toUpperCase() === "MONTH") {
+        dateFormat = "%Y-%m";
+    }
+
+    try {
+        const query = `
+            SELECT 
+                SUM(calories) AS total_calories,
+                AVG(calories) AS average_calories_per_entry,
+                DATE_FORMAT(record_date, ?) AS period
+            FROM MEAL_LOG_TB
+            WHERE record_date BETWEEN ? AND ?
+            GROUP BY period
+            ORDER BY period
+        `;
+        db.execute(
+            query,
+            [dateFormat, startDateTime, endDateTime],
+            (err, rows) => {
+                if (err) {
+                    return next({
+                        code: "SERVER_INTERNAL_ERROR",
+                    });
+                }
+
+                // 결과값 생성
+                const results = rows.map((row) => {
+                    const days = calculateDays(period, row.period);
+                    return {
+                        period: row.period,
+                        total_calories: row.total_calories,
+                        average_calories_per_day: row.total_calories / days,
+                        average_calories_per_entry:
+                            row.average_calories_per_entry,
+                        days: days,
+                    };
+                });
+
+                return res.success({
+                    userId: userId,
+                    count: rows.length,
+                    chartData: results,
+                });
+            }
+        );
+    } catch (e) {
+        return next({
+            code: "SERVER_INTERNAL_ERROR",
+        });
+    }
+});
 
 module.exports = router;
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -7,15 +7,15 @@ router.get("/", (req, res) => {
 
 router.get("/mypage", (req, res) => {
     res.render("mypage");
-});//김진
+}); //김진
 
 router.get("/login", (req, res) => {
     res.render("login");
-});//김진
+}); //김진
 
 router.get("/join", (req, res) => {
     res.render("join");
-});//김진
+}); //김진
 
 router.get("/recipe", (req, res) => {
     res.render("recipe");
@@ -24,8 +24,13 @@ router.get("/recipe", (req, res) => {
 router.get("/sugardiary", (req, res) => {
     res.render("sugardiary");
 });
-router.get("/recipe_details", (req,res)=> {
-    res.render("recipe_details")
-})
+
+router.get("/recipe_details", (req, res) => {
+    res.render("recipe_details");
+});
+
+router.get("/report/:type", (req, res) => {
+    res.render("report");
+});
 
 module.exports = router;

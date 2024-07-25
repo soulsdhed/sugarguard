3bb9a1f12fc79aafa2f7bbda229370664d98094b
JJS <jjs0601015@yahoo.com>
2024-07-25 17:19:29 +0900

    1

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -15,25 +15,28 @@ router.get("/login", (req, res) => {
 
 router.get("/join", (req, res) => {
     res.render("join");
-});//김진
+}); //김진
 
 router.get("/recipe", (req, res) => {
     res.render("recipe");
 }); // 풍규
 
 router.get("/sugardiary", (req, res) => {
-    res.render("sugardiary"); 
+    res.render("sugardiary");
 }); // 풍규
 
-router.get("/mealrecord", (req,res)=> {
-    res.render("mealrecord") ;
-}); // 풍규 
+router.get("/mealrecord", (req, res) => {
+    res.render("mealrecord");
+}); // 풍규
 
-router.get("/recipe_details", (req,res)=> {
+router.get("/recipe_details", (req, res) => {
     res.render("recipe_details");
 });
 router.get("/report/:type", (req, res) => {
     res.render("report");
+});
+router.get("/recipe_list", (req, res) => {
+    res.render("recipe_list");
 });
 
 module.exports = router;

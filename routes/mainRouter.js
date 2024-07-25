2c6f372117583ecefdc22c94e099cdac7380b7f0
shinju <soulsdhed@gmail.com>
2024-07-25 19:33:34 +0900

    merge

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -23,21 +23,12 @@ router.get("/recipe", (req, res) => {
 
 router.get("/sugardiary", (req, res) => {
     res.render("sugardiary");
-<<<<<<< HEAD
 });
-=======
-}); // 풍규
-
-router.get("/mealrecord", (req, res) => {
-    res.render("mealrecord");
-}); // 풍규
-
->>>>>>> 3bb9a1f12fc79aafa2f7bbda229370664d98094b
 router.get("/recipe_details", (req, res) => {
     res.render("recipe_details");
 });
-router.get("/mealrecord", (req,res)=> {
-    res.render("mealrecord") ;
+router.get("/mealrecord", (req, res) => {
+    res.render("mealrecord");
 }); // 풍규 
 router.get("/report/:type", (req, res) => {
     res.render("report");

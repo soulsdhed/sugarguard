4b9ef93f81475831ca8d744088ad7d34cecb07ac
SPG <vndrb123@naver.com>
2024-07-25 17:52:23 +0900

    merge

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -22,22 +22,14 @@ router.get("/recipe", (req, res) => {
 }); // 풍규
 
 router.get("/sugardiary", (req, res) => {
-<<<<<<< HEAD
-    res.render("sugardiary"); 
-}); // 풍규
-
-router.get("/mealrecord", (req,res)=> {
-    res.render("mealrecord") ;
-}); // 풍규 
-
-router.get("/recipe_details", (req,res)=> {
-=======
     res.render("sugardiary");
 });
 router.get("/recipe_details", (req, res) => {
->>>>>>> 34e0f4cd27d7cec9315394e481dc32000e7ab4f2
     res.render("recipe_details");
 });
+router.get("/mealrecord", (req,res)=> {
+    res.render("mealrecord") ;
+}); // 풍규 
 router.get("/report/:type", (req, res) => {
     res.render("report");
 });
